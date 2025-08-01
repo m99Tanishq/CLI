76f8752fea425e365e32da6ea0f71760cdd3ae16
m99Tanishq <21bcs164@ietdavv.edu.in>
2025-08-01 11:46:00 +0530

    fix: plugin.xml whitespace issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,9 @@ repositories {
     mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2023.3.5")
-    type.set("IC") // Target IDE Platform
+    type.set("IC") 
 
     plugins.set(listOf(
         "com.intellij.java",
@@ -28,7 +26,6 @@ dependencies {
 }
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
